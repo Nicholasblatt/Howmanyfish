413629ea5ef7f09b418bef954f23a61f00a1051e
Nicholas Blatt <nicholasblatt@gmail.com>
2020-08-01 03:18:28 -0700

    cleaned up unused folders

--- Scripts/InitMultiSelect.js
+++
@@ -1,8 +0,0 @@
-var select = new MSFmultiSelect(document.querySelector("#religion"), {
-  onChange: function (checked, value, instance) {
-    console.log(checked, value, instance);
-  },
-  selectAll: true,
-  appendTo: "#test",
-  //readOnly:true
-});
